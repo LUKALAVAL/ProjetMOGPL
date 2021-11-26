8436eac6ed0ba9731017b051ac8f0ed0f0ae528d
LUKA <lukalavalferk@gmail.com>
2021-11-26 17:38:05 +0100

    improve type4

--- src/main.py
+++ src/main.py
@@ -1,16 +1,7 @@
 from interp import *
-from transformation import transform
 from paths import *
 
-graph = transform(interp_file('../graphs/test.txt'))
-# for s,v in graph.items():
-#     print(s,v)
+G = interp_file('../graphs/test.txt')
 
-t4 = type4('A','G',graph)
+t4 = type4('A','F',G)
 print(t4)
-
-
-# l1,l2 = interp_input()
-# print(l1)
-# print(l2)
-# print(check_graph(l1,l2))
--- src/paths.py
+++ src/paths.py
@@ -1,6 +1,49 @@
 from math import inf
 
+def transform(G):
+    '''
+    Transforms G into G_
+    '''
+    ### Initialisation
+    sommets,arcs = G
+    G_ = {}
+    Vin = {}
+    Vout = {}
+    for s in sommets:
+        Vin[s] = []
+        Vout[s] = []
+
+    ### Create V
+    for a in arcs:
+        Vout[a.u] += [(a.u,a.t)]
+        Vin[a.v] += [(a.v,a.t+a.l)]
+    for s in sommets:
+        Vout[s] = list(set(Vout[s]))
+        Vin[s] = list(set(Vin[s]))
+    V = {k: Vin.get(k, 0) + Vout.get(k, 0) for k in set(Vin)}
+
+    ### Create G_
+    for v in V.values():
+        v.sort()
+        length = len(v)
+        for i in range(length-1):
+            G_[v[i]] = {v[i+1] : 0}
+        G_[v[length-1]] = {}
+
+    for a in arcs:
+        if (a.u,a.t) in G_.keys():
+            G_[(a.u,a.t)][(a.v,a.t+a.l)] = a.l
+        else:
+            G_[(a.u,a.t)] = {(a.v,a.t+a.l) : a.l}
+
+    return G_
+
+
+
 def dijkstra(s_dep,graph):
+    '''
+    Finds the shortest path from s_dep to every other points
+    '''
     s_kw = { s_dep : [0,[s_dep]] }
     s_unkw = { s : [inf,None] for s in graph if s != s_dep }
 
@@ -22,12 +65,16 @@ def dijkstra(s_dep,graph):
     return s_kw
 
 def dijk_to_path(dijk_path):
-    path = [None]
+    '''
+    Convert a path given from Dijkstra into a "normal" path
+    '''
+    dijk_path.reverse()
+    path = [(None,None)]
     for c in dijk_path:
-        s = c[0]
-        if s != path[-1]:
-            path += [s]
-    return path[1:]
+        if c[0] != path[-1][0]:
+            path += [c]
+    path.reverse()
+    return path[:-1]
 
 def type1(graph):
     pass
@@ -38,16 +85,26 @@ def type2():
 def type3():
     pass
 
-def type4(s_start,s_finish,graph):
+def type4(s_start,s_finish,G):
+    '''
+    Returns the shortest path (type4) from s_start to s_finish
+    '''
+    ### Transform G into G_
+    G_ = transform(G)
+
+    ### Initialisation
     s_dep = (s_start,inf)
-    for k in graph.keys():
+    for k in G_.keys():
         if k[0] == s_start and k[1] < s_dep[1]:
             s_dep = k
 
     if s_dep[1] == inf:
-        return
+        return []
 
-    paths = dijkstra(s_dep,graph)
+    ### Shortest path algorithm
+    paths = dijkstra(s_dep,G_)
+
+    ### Return the shortest path
     path = []
     val = inf
     for s_arr,ch in paths.items():
