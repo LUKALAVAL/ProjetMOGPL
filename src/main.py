d7ae7af2f8c5c6e38d2312d86abc675ac8870403
LUKA <lukalavalferk@gmail.com>
2021-11-27 16:15:13 +0100

    add pathfinding algorithm for type4 with gurobi

--- src/main.py
+++ src/main.py
@@ -1,14 +1,18 @@
 from interp import *
 from paths import *
+import gurobi as g
 
-G = interp_file('../graphs/test2.txt')
+G = interp_file('../graphs/test.txt')
+
+start = 'A'
+end = 'G'
 
 print("\nt1")
-t1 = type1('A','I',G)
+t1 = type1(start,end,G)
 print(t1)
 
 print("\nt2")
-t2 = type2('A','I',G)
+t2 = type2(start,end,G)
 print(t2)
 
 print("\nt3")
@@ -16,5 +20,9 @@ t3 = type3()
 print(t3)
 
 print("\nt4")
-t4 = type4('A','I',G)
+t4 = type4(start,end,G)
 print(t4)
+
+print("\nt4 gurobi")
+t4g = g.type4(start,end,G)
+print(t4g)
