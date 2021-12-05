978b6b21af72ce2f6e6c7bbc9ace84ac18db298f
LUKA <lukalavalferk@gmail.com>
2021-12-05 18:56:32 +0100

    ready pour rendu

--- src/interp.py
+++ src/interp.py
@@ -1,6 +1,9 @@
 from Arc import Arc
 
 def str_to_arc(arc_str):
+    '''
+    Retourne l'arc associé à la chaine de caractère donnée en argument
+    '''
     u,v,t,l = "","","",""
     i = 1
     while arc_str[i] != ',':
@@ -21,35 +24,37 @@ def str_to_arc(arc_str):
     return Arc(u,v,int(t),int(l))
 
 def interp_file(path):
+    '''
+    Interprete le fichier passé en argument en renvoyant le graphe associé
+    '''
     f = open(path,'r')
     n = int(f.readline())
     m = int(f.readline())
     sommets = []
     arcs = []
+    # Pour les sommets
     for i in range(n):
         sommets += [f.readline()[:-1]]
+    # Pour les arcs
     for i in range(m):
         arcs += [str_to_arc(f.readline())]
     f.close()
     return sommets, arcs
 
 def interp_input():
+    '''
+    Construction du graphe depuis le terminal en demandant à l'utilisateur. Renvoie le graphe construit
+    '''
     sommets = []
     arcs = []
     n = int(input("Nombre de sommets (int) : "))
     m = int(input("Nombre d'arcs (int) : "))
+    # Récupération des sommets
     for i in range(n):
         s = input("Nom du sommet " + str(i) + " : ")
         sommets += [s]
+    # Récupération des arcs
     for i in range(m):
         a = input("Arc " + str(i) + " (u,v,t,lambda) : ")
         arcs += [str_to_arc(a)]
     return sommets, arcs
-
-def check_graph(sommets, arcs):
-    if len(sommets) != len(set(sommets)):
-        return False
-    for a in arcs:
-        if not (a.u in sommets) or not (a.v in sommets):
-            return False
-    return True
--- src/main.py
+++ src/main.py
@@ -1,62 +1,26 @@
-from paths import *
-import gurobi as g
-from tests import performance
-import matplotlib.pyplot as plt
-from algo import algo
-from interp import interp_file
+import algo as d # algorithmes basés sur Dijkstra
+import recherches_BFS as b # algorithmes basés sur BFS
+import gurobi as g # algorithmes basés sur Gurobi
+from interp import interp_file, interp_input
 
-ts = -1
-te = 1000
-tests_s = [('0','4',ts,te,'../graphs_s/g3.txt')
-,('0','4',ts,te,'../graphs_s/g4.txt')
-,('0','4',ts,te,'../graphs_s/g5.txt')
-,('0','4',ts,te,'../graphs_s/g6.txt')
-,('0','4',ts,te,'../graphs_s/g7.txt')
-,('0','4',ts,te,'../graphs_s/g8.txt')
-,('0','4',ts,te,'../graphs_s/g9.txt')
-,('0','4',ts,te,'../graphs_s/g10.txt')
-,('0','4',ts,te,'../graphs_s/g11.txt')
-,('0','4',ts,te,'../graphs_s/g12.txt')
-,('0','4',ts,te,'../graphs_s/g13.txt')
-,('0','4',ts,te,'../graphs_s/g14.txt')
-,('0','4',ts,te,'../graphs_s/g15.txt')
-,('0','4',ts,te,'../graphs_s/g16.txt')
-,('0','4',ts,te,'../graphs_s/g17.txt')
-,('0','4',ts,te,'../graphs_s/g18.txt')
-,('0','4',ts,te,'../graphs_s/g19.txt')
-,('0','4',ts,te,'../graphs_s/g20.txt')
-]
+graph_name = 'diff.txt'
+start = 'A'
+end = 'C'
+ts = 0
+te = 20
 
-list_test = tests_s
-symb = "--"
+G = interp_file('../graphs/' + graph_name)
 
-perfT1 = performance(type1,list_test)
-perfT2 = performance(type2,list_test)
-perfT3 = performance(type3,list_test)
-perfT4 = performance(type4,list_test)
-# perfG4 = performance(g.type4,list_test)
-# perf_algo = performance(algo,list_test)
+dt1,dt2,dt3,dt4 = d.algo(start,end,G,ts,te)
+print("Dijkstra type1 :", dt1)
+print("Dijkstra type2 :", dt2)
+print("Dijkstra type3 :", dt3)
+print("Dijkstra type4 :", dt4)
 
-l_nb_sommets = [5,14,19,34,42,59,80,97,118,132,144,184,214,244,279,305,358,379]
+gt4 = g.type4(start,end,G,ts,te)
+print("Gurobi type4 :", gt4)
 
-plt.plot(l_nb_sommets,perfT1,symb)
-plt.plot(l_nb_sommets,perfT2,symb)
-plt.plot(l_nb_sommets,perfT3,symb)
-plt.plot(l_nb_sommets,perfT4,symb)
-# plt.plot(perfG4,l_nb_sommets,symb)
-# plt.plot(perf_algo,symb)
-
-plt.legend(["T1", "T2", "T3", "T4", "G4"])
-# plt.legend(["T4", "G4"])
-
-plt.show()
-
-# G = interp_file("../graphs/test0.txt")
-#
-# p1,p2,p3,p4 = algo('0','20',G,0,20)
-# pg = g.type4('0','20',G,0,20)
-# print("p1 :",p1,"\n")
-# print("p2 :",p2,"\n")
-# print("p3 :",p3,"\n")
-# print("p4 :",p4,"\n")
-# print("pg :",pg,"\n")
+bt1 = b.type1(start,end,G,ts,te)
+bt2 = b.type2(start,end,G,ts,te)
+print("BFS type1 :", bt1)
+print("BFS type2 :", bt2)
